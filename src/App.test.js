f9d91934bee4d4a09d8296d1c49a607b9407ff69
ejiwen <skibnewrim@gmail.com>
2021-07-22 02:35:37 -0700

    create test using react testing library

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title header element', () => {
   render(<App />);
   const titleElement = screen.getByText(/Front-end capstone project for Hack Reactor/i);
   expect(titleElement).toBeInTheDocument();
