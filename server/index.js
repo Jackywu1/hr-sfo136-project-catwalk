8286620719884cd5cafb13b056fa47a87b2af416
Luis Gonzalez <luis.a.gonzalez@live.com>
2021-07-23 13:46:16 -0700

    Set up proxy server for HTTP requests to the Atelier API

--- server/index.js
+++ server/index.js
@@ -1,17 +1,37 @@
 /* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+const axios = require('axios');
 const express = require('express');
 const path = require('path');
-
-const port = 3000;
+require('dotenv').config();
 
 const app = express();
+const port = 3000;
+const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo';
+const headers = {
+  'User-Agent': 'request',
+  Authorization: process.env.GITHUB_AUTH_TOKEN,
+};
+const atelier = axios.create({ baseURL, headers });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use((req, res, next) => {
+  atelier[req.method.toLowerCase()](req.originalUrl, req.body)
+    .then((response) => next({ error: null, response }))
+    .catch((error) => next({ error, response: null }));
+});
+
+app.use(({ error, response }, req, res, next) => {
+  if (error) {
+    res.status(error.response.status).send(error.message);
+  } else {
+    res.status(response.status).send(response.data);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Project Catwalk listening on http://localhost:${port}`);
 });
