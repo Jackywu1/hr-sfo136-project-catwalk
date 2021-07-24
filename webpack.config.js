6182e5fbf9250057813ea98e4b7265ab743ae11c
Luis Gonzalez <luis.a.gonzalez@live.com>
2021-07-23 21:34:25 -0700

    Removed default build mode from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const SRC_DIR = path.join(__dirname, 'client', 'src');
 const DIST_DIR = path.join(__dirname, 'client', 'dist');
 
 module.exports = {
-  mode: 'development',
   entry: path.join(SRC_DIR, 'index.jsx'),
   output: {
     filename: 'bundle.js',
