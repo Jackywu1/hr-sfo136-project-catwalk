243bf8a89b4a4c918973fcef352e1c34dff35508
Luis Gonzalez <luis.a.gonzalez@live.com>
2021-07-25 16:59:56 -0700

    Additional configuration changes to allow React component testing

--- webpack.config.js
+++ webpack.config.js
@@ -5,38 +5,20 @@ const DIST_DIR = path.join(__dirname, 'client', 'dist');
 
 module.exports = {
   entry: path.join(SRC_DIR, 'index.jsx'),
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR,
-  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)?$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', { targets: { node: 'current' } }],
-              '@babel/preset-react',
-            ],
-            plugins: [
-              [
-                '@babel/plugin-transform-runtime',
-                {
-                  absoluteRuntime: false,
-                  corejs: false,
-                  helpers: true,
-                  regenerator: true,
-                  version: '^7.14.8',
-                },
-              ],
-              ['babel-plugin-styled-components'],
-            ],
-          },
-        },
+        use: ['babel-loader'],
       },
     ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
   },
 };
