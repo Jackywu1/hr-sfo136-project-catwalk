c214ed3e82d0b0fe034a6b4b90e8bc940000fb0f
Luis Gonzalez <luis.a.gonzalez@live.com>
2021-07-26 15:44:55 -0700

    Refactored API helper functions to use async/await

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import StarRating from './shared/StarRating';
+import React, { useState, useEffect } from 'react';
+import { api } from '../helpers';
 
 export default function App() {
+  const [product, setProduct] = useState({});
+  const [productInfo, setProductInfo] = useState({});
+  const [reviews, setReviews] = useState({});
+  const [reviewsMeta, setReviewsMeta] = useState({});
+
+  useEffect(() => {
+    api.getAllProducts()
+      .then(({ data }) => setProduct(data[0]))
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    if (product.id) {
+      api.getProductInfo(product.id)
+        .then(({ data }) => setProductInfo(data))
+        .catch((error) => console.error(error));
+    }
+  }, [product]);
+
+  useEffect(() => {
+    if (product.id) {
+      api.getReviews(product.id, 'newest')
+        .then(({ data }) => setReviews(data))
+        .catch((error) => console.error(error));
+    }
+  }, [product]);
+
+  useEffect(() => {
+    if (product.id) {
+      api.getReviewsMeta(product.id)
+        .then(({ data }) => setReviewsMeta(data))
+        .catch((error) => console.error(error));
+    }
+  }, [product]);
+
   return (
     <div className="App">
-      <StarRating color="black" rating={3.8} size="3em" />
+      {console.log(product, productInfo, reviews, reviewsMeta)}
     </div>
   );
 }
