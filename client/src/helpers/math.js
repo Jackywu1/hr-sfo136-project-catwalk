2d447a4c8a954fa6a5dcffe643702adb64705e18
Luis Gonzalez <luis.a.gonzalez@live.com>
2021-07-27 10:49:14 -0700

    Added math helper functions

---
+++ client/src/helpers/math.js
@@ -0,0 +1,19 @@
+module.exports = {
+  getAverageRating: (ratings) => {
+    let ratingsSum = 0;
+    let ratingsCount = 0;
+    for (const entries of Object.entries(ratings)) {
+      const [rating, count] = entries.map(Number) 
+      ratingsSum += rating * count;
+      ratingsCount += count;
+    }
+    const averageRating = ratingsSum / ratingsCount;
+    return averageRating;
+  },
+  normalizeRating: (rating) => {
+    let normalizedRating = (Math.round(rating * 4) / 4);
+    normalizedRating += (normalizedRating % 1 === 0.25) ? 0.08 : 0;
+    normalizedRating -= (normalizedRating % 1 === 0.75) ? 0.08 : 0;
+    return normalizedRating;
+  }
+};
