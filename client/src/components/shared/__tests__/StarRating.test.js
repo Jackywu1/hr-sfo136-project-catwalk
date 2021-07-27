8c9ab7fbd930b4daa1185eec6ace013b571344d5
Luis Gonzalez <luis.a.gonzalez@live.com>
2021-07-26 19:19:44 -0700

    Refactored tests

--- client/src/components/shared/__tests__/StarRating.test.js
+++ client/src/components/shared/__tests__/StarRating.test.js
@@ -7,7 +7,7 @@ afterEach(cleanup);
 describe('StarRating', () => {
   it('renders the component', () => {
     const { container } = render(<StarRating color="black" rating={3.8} size="3em" />);
-    expect(container.getElementsByClassName('StarsOuter').length).toBe(1);
-    expect(container.getElementsByClassName('StarsInner').length).toBe(1);
+    expect(container.querySelector('.StarsOuter')).toBeInTheDocument();
+    expect(container.querySelector('.StarsInner')).toBeInTheDocument();
   });
 });
